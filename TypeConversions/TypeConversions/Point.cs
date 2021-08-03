e9bafc57fdf6dcc7a6369af3544d6a67ce02ef73
Florin Lazar <florin.lazar.it2014@gmail.com>
2021-08-03 19:06:33 +0300

    Implement implicit and explicit conversions; Sample with other operator overloading

---
+++ TypeConversions/TypeConversions/Point.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace TypeConversions
+{
+    public class Point
+    {
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int X { get; }
+
+        public int Y { get; }
+
+        public static implicit operator string(Point p)
+        {
+            return $"({p.X},{p.Y})";
+        }
+
+        public static explicit operator Point(string coords)
+        {
+            if (string.IsNullOrWhiteSpace(coords))
+            {
+                throw new FormatException();
+            }
+
+            string sanitized = coords.Replace("(", string.Empty)
+                                     .Replace(")", string.Empty);
+
+            string[] parts = sanitized.Split(",", StringSplitOptions.RemoveEmptyEntries);
+            if (parts.Length != 2)
+            {
+                throw new FormatException();
+            }
+
+            string xCoord = parts[0];
+            string yCoord = parts[1];
+
+            if (!int.TryParse(xCoord, out int xCoordAsInt))
+            {
+                throw new FormatException();
+            }
+
+            if (!int.TryParse(yCoord, out int yCoordAsInt))
+            {
+                throw new FormatException();
+            }
+
+            return new Point(xCoordAsInt, yCoordAsInt);
+        }
+
+        public static Point operator +(Point p1, Point p2)
+        {
+            return new Point(p1.X + p2.X, p1.Y + p2.Y);
+        }
+    }
+}
---
+++ TypeConversions/TypeConversions/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TypeConversions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(100, 200);
+            string coords = p1;
+            Console.WriteLine(coords);
+
+            Point p2 = (Point)coords;
+            Console.WriteLine($"Point: X={p2.X}, Y={p2.Y}");
+
+            Point p3 = p1 + p2;
+            Console.WriteLine($"Point Sum: X={p3.X}, Y={p3.Y}");
+        }
+    }
+}
